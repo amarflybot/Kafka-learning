3b2a8424fe07103160a69d61dc4c3c3577fa1dd3
Kumar <V691139@ASIAPAC.AD.JPMORGANCHASE.com>
2016-11-25 17:02:14 +0530

    Initial commit

---
+++ src/main/java/com/example/CustomObject.java
@@ -0,0 +1,42 @@
+package com.example;
+
+/**
+ * Created by V691139 on 11/25/2016.
+ */
+public class CustomObject {
+
+    private String name;
+    private Integer roll;
+
+    public CustomObject() {
+    }
+
+    public CustomObject(String name, Integer roll) {
+        this.name = name;
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getRoll() {
+        return roll;
+    }
+
+    public void setRoll(Integer roll) {
+        this.roll = roll;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomObject{" +
+                "name='" + name + '\'' +
+                ", roll=" + roll +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/KafkaProducerTest.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+/**
+ * Created by V691139 on 11/21/2016.
+ */
+public class KafkaProducerTest {
+
+    public static void main(String[] args) {
+
+        Properties properties = new Properties();
+        properties.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG , "localhost:9092,localhost:9093,localhost:9094");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG , "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG , "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String,String> kafkaProducer = new KafkaProducer(properties);
+
+        System.out.println(kafkaProducer.metrics());
+
+        for(int i=0;i<150;i++){
+            kafkaProducer.send(new ProducerRecord("my-topics","Key-"+Integer.toString(i),"Value-"+Integer.toString(i)));
+        }
+        kafkaProducer.close();
+    }
+
+}
